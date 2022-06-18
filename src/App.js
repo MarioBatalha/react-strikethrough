9e808c1814bd667a77ab97ccec0c56f567d1900e
MarioBatalha <mariobatalha9@gmail.com>
2022-06-18 21:07:46 +0100

    feat: add source code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import './App.css';
+import React, { useState } from 'react';
+
+import Form from './components/form';
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const toggleComplete = index => {
+    setTodos(
+      todos.map((todo, currentValue) =>
+        currentValue === index
+          ? {
+              ...todo,
+              complete: !todo.complete,
+            }
+          : todo
+      )
+    );
+  };
   return (
     <div className="App">
+      <h1>Todo list</h1>
+      <Form 
+      onSubmit={text => setTodos([{ text, complete: false }, ...todos])}
+      />
+      {
+        todos.map(({text, complete}, index) => {
+          return (
+            <div key={index} onClick={() => toggleComplete(index)}
+            style={{
+              textDecoration: complete ? "line-through" : ""
+            }}
+            >{text}</div>
+          )
+        })
+      }
+      <button onClick={() => setTodos([])}>Reset</button>
     </div>
   );
 }
---
+++ src/components/form.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const useInputValue = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+
+  return {
+    value,
+    onChange: (e) => setValue(e.target.value),
+    resetValue: () => setValue(""),
+  };
+};
+
+export default ({ onSubmit }) => {
+  const { resetValue, ...text } = useInputValue("");
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit(text.value);
+          resetValue();
+        }}
+      >
+        <input {...text} />
+      </form>
+    </div>
+  );
+};
